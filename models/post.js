7bfa4097b931c8c5868294abcd4270968760fe57
ABNmmd <moha.ww.ha@gmail.com>
2024-07-22 15:31:03 +0100

    update the post model

--- models/post.js
+++ models/post.js
@@ -2,22 +2,39 @@ const mongoose = require('mongoose');
 
 const PostSchema = mongoose.Schema(
     {
+        _id: mongoose.Schema.Types.ObjectId,
+        authorId: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+        },
         title: {
             type: String,
             required: true,
         },
-
         content: {
             type: String,
             required: true,
         },
-
-        author: {
-            type: String,
-            required: true,
-        },
-
+        tags: [
+            {
+                type: String,
+            }
+        ],
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+            }
+        ],
+        dislikes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+            }
+        ],
         createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
             type: Date,
             default: Date.now,
         },
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema(
+    {
+        _id: mongoose.Schema.Types.ObjectId,
+        
+    }
+);
