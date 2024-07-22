856bf16748b7a0449d89c03ce887565daf3ed735
ABNmmd <moha.ww.ha@gmail.com>
2024-07-22 15:44:02 +0100

    add the user model

--- models/user.js
+++ models/user.js
@@ -3,6 +3,40 @@ const mongoose = require('mongoose');
 const UserSchema = mongoose.Schema(
     {
         _id: mongoose.Schema.Types.ObjectId,
-        
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        profilePicture: {
+            type: String,
+            required: false,
+        },
+        bio: {
+            type: String,
+            required: false,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        updatedAt: {
+            type: Date,
+            default: Date.now,
+        },
+    },
+    {
+        timestamps: true,
     }
 );
+
+const User = mongoose.model("user", UserSchema);
+
+module.exports = User;
