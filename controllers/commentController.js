ced28fadf0c30814740ae8f009a29741fd248cb1
ABNmmd <moha.ww.ha@gmail.com>
2024-07-26 00:01:12 +0100

    comment controller: starting creat comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,2 +1,10 @@
 const Post = require("../models/comment");
 
+//creat comment
+const creatComment = async (req, res) => {
+    try {
+        const { content, postId } = req.body;
+    }catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+}
