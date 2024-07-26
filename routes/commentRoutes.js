8b381450e5a54e926af17289cacf0039589c3e32
ABNmmd <moha.ww.ha@gmail.com>
2024-07-26 22:30:54 +0100

    comment routes

---
+++ routes/commentRoutes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { createComment, getComments, updateComment, deleteComment } = require('../controllers/commentController.js');
+
+const authMiddleware = require('../middleware/authMiddleware.js');
+
+const router = express.Router();
+
+
+
+// create comment
+router.post('/', authMiddleware, createComment);
+
+// get comment
+router.get('/', getComments);
+
+// update comment
+router.put('/:id', authMiddleware, updateComment);
+
+// delete comment
+router.delete('/:id', authMiddleware, deleteComment);
+
+module.exports = router;
