8173fc27a4340313ad56d38645d6788abd322746
ABNmmd <moha.ww.ha@gmail.com>
2024-07-26 16:20:29 +0100

    comment controller: checking the post exist before creating the comment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,9 +1,17 @@
-const Post = require("../models/comment");
+const Comment = require("../models/comment");
+const Post = require("../models/post");
 
 //creat comment
 const creatComment = async (req, res) => {
     try {
         const { content, postId } = req.body;
+        const userId = req.session.userId;
+        if(!content || !postId) {
+            return res.status(400).json({ message: "Content and Post ID are required." });
+        }
+
+        const post = await Post.findById(postId);
+        if(!)
     }catch(error) {
         res.status(500).json({ message: error.message });
     }
