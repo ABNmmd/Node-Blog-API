d214ad9be243eb1fc8ef311758a3388d2d037803
ABNmmd <moha.ww.ha@gmail.com>
2024-07-26 19:50:40 +0100

    comment controller: deleteComments error catching

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -66,5 +66,9 @@ const updateComment = async (req, res) => {
 
 // delete a comment
 const deleteComment = async (req, res) => {
-    
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
