29f2fd6cf21af08669d18f2c3cc49e43a99ea15d
ABNmmd <moha.ww.ha@gmail.com>
2024-07-27 20:33:01 +0100

    user controller : getUser geting the user data by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 // get user profile
 const getUser = async (req, res) => {
     try {
+        const userId = req.session.userId;
+        const user = await User.findById({ userId }).select("-password");
         
     } catch (error) {
         res.status(500).json({ error: error.message });
