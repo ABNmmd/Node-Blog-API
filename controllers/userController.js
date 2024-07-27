cefc34099e1b54d9d2bbc95732cd1299a174b643
ABNmmd <moha.ww.ha@gmail.com>
2024-07-27 20:19:49 +0100

    user controller : getUser error catching

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,5 +2,9 @@ const User = require("../models/user");
 
 // get user profile
 const getUser = async (req, res) => {
-    
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
 }
