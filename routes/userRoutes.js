13f515ab30b79af16e85d229be469a39135e5d38
ABNmmd <moha.ww.ha@gmail.com>
2024-07-27 21:03:29 +0100

    user Routes requiring what we need

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,6 @@
 const express = require('express');
+const { getUser, updateUser } = require('../controllers/userController.js');
+
+const authMiddleware = require('../middleware/authMiddleware.js');
+
+const router = express.Router();
