2641d2d4eef9b08636a790f443d17b3de974db9b
ABNmmd <moha.ww.ha@gmail.com>
2024-07-27 20:35:04 +0100

    user controller : getUser cheaking for the user existence

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,11 @@ const getUser = async (req, res) => {
     try {
         const userId = req.session.userId;
         const user = await User.findById({ userId }).select("-password");
-        
+
+        if(!user){
+            return res.status(404).json({ message: 'User not found' });
+        }
+
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
