0dddaad24c2c025c90bb7840c695e7ad43b77bd9
ABNmmd <moha.ww.ha@gmail.com>
2024-07-27 22:02:35 +0100

    setup the validator

---
+++ utils/validators.js
@@ -0,0 +1,19 @@
+const { body, validationResult } = require('express-validator');
+
+const validateUserProfileUpdate = [
+    body('username').optional().isString().isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+    body('email').optional().isEmail().withMessage('Invalid email address'),
+    body('profilePicture').optional().isURL().withMessage('Profile picture must be a valid URL'),
+    body('bio').optional().isString().isLength({ max: 200 }).withMessage('Bio must be less than 200 characters long'),
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        next();
+    },
+];
+
+module.exports = {
+    validateUserProfileUpdate,
+};
