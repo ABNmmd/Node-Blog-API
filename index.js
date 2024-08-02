8cb91910d591d461822a114c621e5f5af9044980
ABNmmd <moha.ww.ha@gmail.com>
2024-08-02 23:07:54 +0100

    adding the cors

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-app.use(cors());
-
 const app = express();
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
 
 const store = new MongoDBStore({
     mongoUrl: process.env.MONGODB_URI,
