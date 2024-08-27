efdd9910046ff386925740024f96d1c8025da43b
ABNmmd <moha.ww.ha@gmail.com>
2024-08-27 20:51:44 +0100

    handling post interactions

--- controllers/postIntController.js
+++ controllers/postIntController.js
@@ -3,7 +3,14 @@ const Post = require("../models/post");
 // Like a post
 const likePost = async (req, res) => {
     try {
-        
+        const { id } = req.params;
+        const userId = req.session.userId;
+
+        const post = Post.findById(id);
+        if (!post) {
+            return res.status(404).json({ message: 'Post not found' });
+        }
+
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
