4317c8df296e5b4229e9c216a5091f024688e82e
ABNmmd <moha.ww.ha@gmail.com>
2024-08-27 20:47:53 +0100

    handling post interactions

--- controllers/postIntController.js
+++ controllers/postIntController.js
@@ -2,7 +2,11 @@ const Post = require("../models/post");
 
 // Like a post
 const likePost = async (req, res) => {
-    
+    try {
+        
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 }
 
 module.exports = {
