f655df5c8df9c3b96ae8d87bce8839c7415f6e8f
ABNmmd <moha.ww.ha@gmail.com>
2024-08-27 21:15:19 +0100

    handling post interactions

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -19,4 +19,10 @@ router.put('/:id', authMiddleware, updateComment);
 // delete comment
 router.delete('/:id', authMiddleware, deleteComment);
 
+// like a post
+router.put('/:id/like', authMiddleware, likeComment);
+
+// dislike a post
+router.put('/:id/dislike', authMiddleware, dislikeComment);
+
 module.exports = router;
