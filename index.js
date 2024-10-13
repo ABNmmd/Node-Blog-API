45916530de07ba47aed240e72afbae295db7c8df
ABNmmd <moha.ww.ha@gmail.com>
2024-10-13 21:10:52 +0100

    vercel json file added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'https://academia-hub-front-end.vercel.app/'],
+    origin: ['https://academia-hub-front-end.vercel.app/', 'http://localhost:5173'],
     credentials: true,
 };
 app.use(cors(corsOptions));
